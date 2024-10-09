3fb5906e7b974ccb25132958b224834e7ed0fb1c
Parth Kulkarni <parthpsk0517@gmail.com>
2024-10-09 16:02:38 +0530

    modified:   .gitignore

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "ccgroup88393a935": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
